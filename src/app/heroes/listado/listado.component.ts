4e46f301bcf6aa9bcd577d0978f391572324e413
javierMarin97 <javieremarin97@gmail.com>
2022-09-08 18:25:10 -0500

    ngFor y ngIF

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[]=['Spiderman','Ironman','Hulk','Thor','black Widow']
+  heroeBorrado:string=""
+  borrarHeroe(){
+    console.log("Borrando....")
+   this.heroeBorrado=this.heroes.pop()||""
+   console.log(this.heroeBorrado)
+  }
+}
